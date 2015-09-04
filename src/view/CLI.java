8c572c6a0ad8e6a3840bb9fef6dc4515bbad4b00
amit sandak <sandak.amit@gmail.com>
2015-09-05 00:11:03 +0300

    new

---
+++ src/view/CLI.java
@@ -0,0 +1,54 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import controller.Command;
+
+public class CLI {
+	
+	BufferedReader in;
+    PrintWriter out;
+    HashMap<String,Command> commandMap;
+    
+    public CLI(	BufferedReader in,PrintWriter out,HashMap<String,Command> commandMap)
+    {
+    	this.in=in;
+        this.out=out;
+        this.commandMap=commandMap;
+    }
+    
+    public void start()
+    {
+ 
+    new Runnable() {
+		
+		@Override
+		public void run() {
+				try {
+			   String buffer=null;
+			    Command command = null;
+			 while(buffer!="exit")
+			    {
+			    
+						buffer = in.readLine();
+					
+			    	command = commandMap.get(buffer);
+			    	if (command!=null)
+			    		command.doCommand();
+			    	else
+			    		out.println(buffer + " is not reconized command!");
+			    	}
+				}catch (IOException e) {
+						e.printStackTrace();
+					}
+			    	
+			    }
+		
+	}.run();
+   
+    } 
+    
+}
